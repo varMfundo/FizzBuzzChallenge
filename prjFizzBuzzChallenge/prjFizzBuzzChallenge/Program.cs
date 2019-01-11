a34355f7434f02715b9f9326a70784f6f8e249bd
TREG <42043937+varMfundo@users.noreply.github.com>
2019-01-11 10:08:16 +0200

    FizzBuzzChallengeWithIF..THEN

---
+++ prjFizzBuzzChallenge/prjFizzBuzzChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjFizzBuzzChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Fizz = "FIZZ";
+            const string Buzz = "BUZZ";
+
+            int endCount = 15;
+            int count = 1;
+            int fizzNumber = 3;
+            int buzzNumber = 5;
+
+            for(count = 1; count <= endCount; count++)
+            {
+                
+
+                if ((count % fizzNumber == 0) && (count % buzzNumber == 0))
+                {
+                    Console.WriteLine(Fizz+Buzz);
+                }
+                else if (count % fizzNumber == 0)
+                {
+                    Console.WriteLine(Fizz);
+                }
+                else if (count % buzzNumber == 0)
+                {
+                    Console.WriteLine(Buzz);
+                }
+                else
+                  Console.WriteLine(count.ToString());
+            }
+            Console.ReadLine();
+      
+        }
+
+
+    }
+}
